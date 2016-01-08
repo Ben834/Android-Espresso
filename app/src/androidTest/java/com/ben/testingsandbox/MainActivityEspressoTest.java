106020fc01e7a86017bc1548a87f086a1eac0bf7
Ben <benjamin.duvivier@djit.fr>
2016-01-08 15:02:27 +0100

    Simple test + custom action + custom matcher

---
+++ app/src/androidTest/java/com/ben/testingsandbox/MainActivityEspressoTest.java
@@ -0,0 +1,42 @@
+package com.ben.testingsandbox;
+
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityEspressoTest {
+
+    /**
+     * A JUnit {@link Rule @Rule} to launch your activity under test. This is a replacement
+     * for {@link ActivityInstrumentationTestCase2}.
+     * <p>
+     * Rules are interceptors which are executed for each test method and will run before
+     * any of your setup code in the {@link Before @Before} method.
+     * <p>
+     * {@link ActivityTestRule} will create and launch of the activity for you and also expose
+     * the activity under test. To get a reference to the activity you can use
+     * the {@link ActivityTestRule#getActivity()} method.
+     */
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldBeAbleToLaunchMainScreen() {
+        String text = mActivityRule.getActivity().getResources().getString(R.string.main_hello);
+        onView(withText(text)).check(ViewAssertions.matches(isDisplayed()));
+    }
+
+}
