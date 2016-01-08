bb1fdc1d552e7bcd9e4a56cff486c298dc50b953
Ben <benjamin.duvivier@djit.fr>
2016-01-08 19:58:40 +0100

    Added some tests for the screen rotation and the toolbar

--- app/src/androidTest/java/com/ben/testingsandbox/MainActivityEspressoTest.java
+++ app/src/androidTest/java/com/ben/testingsandbox/MainActivityEspressoTest.java
@@ -1,10 +1,18 @@
 package com.ben.testingsandbox;
 
-import android.support.test.espresso.assertion.ViewAssertions;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.app.ActionBar;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.widget.Toolbar;
+
+import com.ben.testingsandbox.custom.matcher.ToolbarTitleMatcher;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,7 +20,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
@@ -33,10 +44,51 @@ public class MainActivityEspressoTest {
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
+    @Before
+    public void checkInitialCount() {
+        String defaultValue = InstrumentationRegistry.getTargetContext().getString(R.string.default_count);
+        onView(withId(R.id.activity_main_count)).check(matches(withText(defaultValue)));
+    }
+
+    @Test
+    public void increment() {
+        //Click on the increment button
+        onView(withId(R.id.activity_main_increment)).perform(click());
+        //Check the counter
+        onView(withId(R.id.activity_main_count)).check(matches(withText("1")));
+    }
+
+    @Test
+    public void incrementTwiceAndRotateScreen() {
+        onView(withId(R.id.activity_main_increment))
+                .perform(click())
+                .perform(click());
+        onView(withId(R.id.activity_main_count)).check(matches(withText("2")));
+
+        rotateScreen();
+
+        onView(withId(R.id.activity_main_count)).check(matches(withText("2")));
+    }
+
+    /**
+     * Checks that we have the right title in the toolbar for this activity.
+     */
     @Test
     public void shouldBeAbleToLaunchMainScreen() {
-        String text = mActivityRule.getActivity().getResources().getString(R.string.main_hello);
-        onView(withText(text)).check(ViewAssertions.matches(isDisplayed()));
+        CharSequence title = InstrumentationRegistry.getTargetContext().getString(R.string.app_name);
+        onView(isAssignableFrom(Toolbar.class)).check(matches(ToolbarTitleMatcher.withToolBarTitle(title)));
+    }
+
+    /**
+     * A simple function for rotating the screen.
+     */
+    private void rotateScreen(){
+        Context context = InstrumentationRegistry.getTargetContext();
+        int orientation = context.getResources().getConfiguration().orientation;
+
+        Activity activity = mActivityRule.getActivity();
+        activity.setRequestedOrientation(orientation == Configuration.ORIENTATION_PORTRAIT ?
+                ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE : ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
 }
--- app/src/main/java/com/ben/testingsandbox/MainActivity.java
+++ app/src/main/java/com/ben/testingsandbox/MainActivity.java
@@ -1,13 +1,53 @@
 package com.ben.testingsandbox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String KEY_COUNT = "count";
+
+    private int count = 0;
+    private TextView countView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.app_name);
+        countView = (TextView) findViewById(R.id.activity_main_count);
+        Button incrementView = (Button) findViewById(R.id.activity_main_increment);
+        incrementView.setOnClickListener(this);
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt(KEY_COUNT, 0);
+            countView.setText(String.valueOf(count));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_COUNT, count);
+    }
+
+    public void increment() {
+        updateCount();
+    }
+
+    private void updateCount() {
+        count += 1;
+        countView.setText(String.valueOf(count));
+    }
+
+    @Override
+    public void onClick(View v) {
+        final int id = v.getId();
+        if(id == R.id.activity_main_increment){
+            increment();
+        }
     }
 }
