8c12d23b6c56fc1349434e2eb87945e96aa396b7
Ben <benjamin.duvivier@djit.fr>
2016-02-12 10:53:04 +0100

    [mockito] comment added

--- mockitobasics/src/test/java/com/ben/mockitobasics/MockitoDagger.java
+++ mockitobasics/src/test/java/com/ben/mockitobasics/MockitoDagger.java
@@ -11,7 +11,7 @@ import static org.mockito.Mockito.when;
 
 /**
  * A simple demonstration of Dagger Mock
- * <p>
+ * <p/>
  * The advantage of this library is that you don't have to create a test {@link CoffeeComponent}
  * and a test {@link dagger.Module} when you want to test with Dagger.
  *
@@ -28,7 +28,13 @@ public class MockitoDagger {
 
     CoffeeManager mCoffeeManager;
 
-    //Module subclass and test component are automatically created.
+    /**
+     * When DaggerMockRule rule is instantiated, it looks for all @Mock annotated fields in your test
+     * class and it replaces them with Mockito mocks if there is a provider method in your module for that class.
+     * <p/>
+     * The rule dynamically creates a new module that overrides {@link CoffeeModule} which returns
+     * the mocks.
+     */
     @Rule
     public DaggerMockRule<CoffeeComponent> rule =
             new DaggerMockRule<>(CoffeeComponent.class, new CoffeeModule())
